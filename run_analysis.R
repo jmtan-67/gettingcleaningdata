6d6860af19daf75cdcae7e61c60a94d6915d2ae6
jmtan <tan.jia.min.1998@gmail.com>
2020-09-22 17:48:40 +0800

    The tidy data set, R script and CodeBook were updated to reflect new changes.

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,6 @@
+library(plyr)
+library(dplyr)
+
 test <- read.table("UCI HAR Dataset/test/X_test.txt")
 train <- read.table("UCI HAR Dataset/train/X_train.txt")
 testlabel <- read.table("UCI HAR Dataset/test/y_test.txt")
@@ -5,31 +8,22 @@ trainlabel <- read.table("UCI HAR Dataset/train/y_train.txt")
 testsubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
 trainsubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
 
-## 1. Merge training and test sets
-data <- rbind(test, train)
-
-## 2. Extract mean and sd for each measurement
-means <- sapply(data, mean)
-sds <- sapply(data, sd)
+## Merge training and test sets
+fulldata <- rbind(test, train)
 
 
-## 4. Descriptive variable names
+## Change column labels to descriptive variable names
 collabels <- as.character(read.table("UCI HAR Dataset/features.txt")[,2])
-table(table(collabels)) # there are 42 instances where there triplicates
-# add _1/2/3 to the triplicated names
-replicates <- unique(collabels[duplicated(collabels)])
-for (i in replicates){
-  indices <- which(collabels == i)
-  collabels[indices[1]] <- paste(i, "_1", sep="")
-  collabels[indices[2]] <- paste(i, "_2", sep="")
-  collabels[indices[3]] <- paste(i, "_3", sep="")
-}
-table(table(collabels)) # no replicates
-names(data) <- collabels
+names(fulldata) <- collabels
 
-## 3. Descriptive activity names
-library(plyr)
-library(dplyr)
+
+## Extract mean and sd for each measurement
+meansd <- grep("[Mm]ean\\(|std\\(", names(fulldata)) ## 66 obs which is right
+data <- select(fulldata, names(fulldata)[meansd])
+table(table(names(data))) # each variable name is unique
+
+
+## Add the activity names
 labels <- rbind(testlabel, trainlabel)
 labels <- as.factor(labels[,1])
 labels <- revalue(labels, c("1"="walking","2"="walking_upstars","3"="walking_downstairs",
